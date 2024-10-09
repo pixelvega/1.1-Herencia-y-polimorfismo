8146e623bed26a8c86d451362535183258a45673
pixelvega <pixelvega.info@gmail.com>
2024-10-09 11:34:50 +0200

    Exercise_2_1: Add getters and setters

--- src/exercise2_1/Telephone.java
+++ src/exercise2_1/Telephone.java
@@ -1,14 +1,30 @@
 package exercise2_1;
 
 public class Telephone {
-	public String brand;
-	public String model;
+	private String brand;
+	private String model;
 
 	public Telephone(String brand, String model) {
 		this.brand = brand;
 		this.model = model;
 	}
 	
+	public String getBrand() {
+		return this.brand;
+	}
+	
+	public String getModel() {
+		return this.model;
+	}
+	
+	public void setBrand(String brand) {
+		this.brand = brand;
+	}
+
+	public void setModel(String model) {
+		this.model = model;
+	}
+
 	public void call(String phoneNumber) {
 		System.out.println("Está llamando al número " + phoneNumber);
 	}
